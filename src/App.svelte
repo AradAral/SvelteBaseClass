79df3b4d8def820d6936f55fc223e835f2a9f02c
Arad Aral <AmirHosseinHmd@outlook.com>
2021-03-06 05:20:24 +0330

    Initial commit.

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Button from "./Button.svelte";
+    import type { SvelteComponent, SvelteComponentTyped } from 'svelte';
+
+    let comp1: SvelteComponentTyped = Button;
+    let comp2: SvelteComponent = Button;
+</script>
+
+<Button label="Submit" />
